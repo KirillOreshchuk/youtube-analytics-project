f6c7fd0ab0eafb6405d612c946c6cf026165c4a6
KirillOreshchuk <oreshchuk.kirill@gmail.com>
2023-09-09 23:33:35 +1000

    modified channel.py, .gitignore, add pyprogect.toml

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -8,7 +8,8 @@ import isodate
 
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+# api_key: str = os.getenv('YT_API_KEY') не работает эта конструкция
+api_key: str = "AIzaSyDzWMCtRnkwIfUSTbjvN9fuPnT2DVNTz1I"
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,19 @@
-
+import os
+from googleapiclient.discovery import build
+from helper.youtube_api_manual import printj
+api_key: str = "AIzaSyDzWMCtRnkwIfUSTbjvN9fuPnT2DVNTz1I"
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
-    def print_info(self) -> None:
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        pass
+
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
+        return printj(channel)
