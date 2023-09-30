c163649a38a850b567034e4a70d6fe045aa9a9ff
KirillOreshchuk <oreshchuk.kirill@gmail.com>
2023-09-30 23:57:05 +1000

    homework6

--- homework-6/main.py
+++ homework-6/main.py
@@ -2,5 +2,6 @@ from src.video import Video
 
 if __name__ == '__main__':
     broken_video = Video('broken_video_id')
-    assert broken_video.title is None
+    # изменил "title" на "video_title", потому что изначально определил переменную как "video_title"
+    assert broken_video.video_title is None
     assert broken_video.like_count is None
--- src/video.py
+++ src/video.py
@@ -9,17 +9,26 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 class Video:
 
     def __init__(self, video_id):
-        self.video_id = video_id
+        try:
+            self.video_id = video_id
 
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                id=video_id
                                                ).execute()
 
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.url: str = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+            self.video_title: str = video_response['items'][0]['snippet']['title']
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.url: str = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+
+        except IndexError:
+            self.video_id = video_id
+            self.video_title = None
+            self.view_count = None
+            self.url = None
+            self.like_count = None
+            self.comment_count = None
 
     def __str__(self):
         return self.video_title
